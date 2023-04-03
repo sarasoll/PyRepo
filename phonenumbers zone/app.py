97a97523c08b5432c21e40569bd85b4ca62117c9
Soll✯ <48865246+sarasoll@users.noreply.github.com>
2023-04-03 15:51:40 +0300

    Update app.py

--- phonenumbers zone/app.py
+++ phonenumbers zone/app.py
@@ -2,5 +2,5 @@
 import phonenumbers
 from phonenumbers import timezone
 
-phone_number = phonenumbers.parse("+966570409960")
-print(timezone.time_zones_for_number(phone_number))
+pnum = phonenumbers.parse("+966570409960")
+print(timezone.time_zones_for_number(pnum))
