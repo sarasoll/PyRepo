bc90dc937642b8c7556fdae1f837241932ff3893
Soll✯ <48865246+sarasoll@users.noreply.github.com>
2023-04-03 21:20:50 +0300

    Update app.py

--- phonenumbers full info/app.py
+++ phonenumbers full info/app.py
@@ -1,9 +1,11 @@
 import phonenumbers
 from phonenumbers import carrier, geocoder, timezone
-mobileno = input("Enter mobile number with country code: ")
-mobileno=phonenumbers.parse(mobileno)
-print(mobileno)
-print(timezone.time_zones_for_number(mobileno))
-print(carrier.name_for_number(mobileno,"en"))
-print(geocoder.description_for_number(mobileno,"en"))
-print("Valid mobile number: ",phonenumbers.is_valid_number(mobileno))
+
+
+pnum= input("Enter mobile number with country code: ")
+pnum=phonenumbers.parse(pnum)
+print(pnum)
+print(timezone.time_zones_for_number(pnum))
+print(carrier.name_for_number(pnum,"en"))
+print(geocoder.description_for_number(pnum,"en"))
+print("Valid mobile number: ",phonenumbers.is_valid_number(pnum))
