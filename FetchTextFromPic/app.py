d7b55b043996878296dc30007741319cb4ebb697
Soll✯ <48865246+sarasoll@users.noreply.github.com>
2023-11-30 12:08:34 +0300

    Update app.py

--- FetchTextFromPic/app.py
+++ FetchTextFromPic/app.py
@@ -1,1 +1,8 @@
+from PIL import Image
 
+import pytesseract
+#I face a problem in installing the "pytesseract" but you can find the solve in the README file 
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+print(pytesseract.image_to_string(Image.open('3.png')))
